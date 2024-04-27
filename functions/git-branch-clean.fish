dbea1e81faf909eeef0f2875bf5544b9bc5b5909
Pierre Schmitz <pierre@archlinux.de>
2024-04-27 10:40:15 +0200

    Validate git repository before launching git-branch-clean

--- functions/git-branch-clean.fish
+++ functions/git-branch-clean.fish
@@ -1,4 +1,15 @@
 function git-branch-clean --description 'Remove branches that no longer exist remotly or are merged locally'
+    if not command -q git
+        echo 'git command not found'
+        fish_command_not_found git
+        return 1
+    end
+
+    if not git status &>/dev/null
+        echo "No valid git repository found"
+        return 1
+    end
+
     git remote prune origin
 
     set -l deleted_branches

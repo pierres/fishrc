c4920c63d2f722bef846cf9b093234a65e592e90
Pierre Schmitz <pierre@archlinux.de>
2024-10-22 11:36:43 +0200

    Remove helix alias as it is no longer used

--- conf.d/fishrc_abbr.fish
+++ conf.d/fishrc_abbr.fish
@@ -2,10 +2,6 @@ if status is-interactive
     abbr lh ls -hAl
     abbr l ls -l
 
-    if not abbr -q hx; and not command -q hx; and command -q helix
-        abbr hx helix
-    end
-
     if command -q just
         abbr jsut just
     end

2319d08e657acb68776a7a7d226a88189126d85e
Pierre Schmitz <pierre@archlinux.de>
2025-08-13 19:01:58 +0200

    Warn when gh is not installed

--- functions/git-branch-main.fish
+++ functions/git-branch-main.fish
@@ -18,8 +18,12 @@ function git-branch-main --description 'Rename the default Git branch from maste
 
     git push -u origin main
 
-    if command -q gh; and string match -q master (gh repo view --jq '.defaultBranchRef.name' --json 'defaultBranchRef' 2>/dev/null)
-        gh repo edit --default-branch main
+    if command -q gh
+        if string match -q master (gh repo view --jq '.defaultBranchRef.name' --json 'defaultBranchRef' 2>/dev/null)
+            gh repo edit --default-branch main
+        end
+    else if git remote -v | string match -q 'github.com'
+        echo "Warning: 'gh' command not found, please change the default branch on GitHub manually." >&2
     end
 
     git push origin --delete master
